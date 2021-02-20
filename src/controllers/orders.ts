70bd5961061c51a8d24328ae791dbddf7a9ece5e
patrickJasonDuque <patpatduque26@gmail.com>
2021-02-20 20:58:24 +0800

    added update pay route

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -39,7 +39,7 @@ export const addOrder: RequestHandler = async (req, res) => {
 // @access Private
 export const getOrdersByUser: RequestHandler = async (req, res) => {
 	try {
-		const orders: OrderType[] = await Order.find({ user: req.user.id });
+		const orders: OrderType[] = await Order.find({ user: req.user.id }).select('-user');
 		res.status(200).json(orders);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
@@ -51,8 +51,42 @@ export const getOrdersByUser: RequestHandler = async (req, res) => {
 // @access Private
 export const getOrdersByAdmin: RequestHandler = async (req, res) => {
 	try {
-		const orders: OrderType[] = await Order.find({});
-		res.status(200).json(orders);
+		const admin: UserType = await User.findById(req.user.id);
+		if (!admin.isAdmin) {
+			res.status(401);
+			throw new Error('Unauthorized user');
+		} else {
+			const orders: OrderType[] = await Order.find({});
+			res.status(200).json(orders);
+		}
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+// @desc Update Order to paid
+// @route PUT /api/orders/:id/pay
+// @access Private
+export const updateOrderToPaid: RequestHandler = async (req, res) => {
+	try {
+		const order: OrderType = await Order.findById(req.params.id);
+
+		if (!order) {
+			res.status(404);
+			throw new Error('No order found');
+		} else {
+			order.isPaid = true;
+			order.paidAt = new Date();
+			order.paymentResult = {
+				id: req.body.id,
+				status: req.body.status,
+				update_time: req.body.update_time,
+				email_address: req.body.payer.email_address
+			};
+
+			const updatedOrder = await order.save();
+			res.status(201).json(updatedOrder);
+		}
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -7,5 +7,6 @@ const router = Router();
 router.post('/', protect, orderController.addOrder);
 router.get('/', protect, orderController.getOrdersByAdmin);
 router.get('/user', protect, orderController.getOrdersByUser);
+router.put('/:id/pay', protect, orderController.updateOrderToPaid);
 
 export default router;
